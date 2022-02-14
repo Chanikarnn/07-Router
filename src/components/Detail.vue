e6bc08ffc47578d2113e01132ef6ec8527c84878
Chanikarnn <95094960+Chanikarnn@users.noreply.github.com>
2022-02-14 23:38:24 +0700

    07

---
+++ src/components/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+      <h1>Product Detail Page</h1>
+      <h2>ID = {{ productid }}</h2>
+      
+  </v-container>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            productid: this.$route.params.id,
+            rate: this.$route.params.Name,
+        };
+        
+    },
+    
+
+    
+
+};
+</script>
+
+<style>
+
+</style>
